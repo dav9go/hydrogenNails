641fc42c2baf75ffeecd5db423ee67612d22ac57
dav9go <david9go@gmail.com>
2023-10-15 16:33:37 +0200

    first commit

---
+++ app/routes/_index.jsx
@@ -0,0 +1,13 @@
+export function meta() {
+  return [
+    {title: 'Hydrogen'},
+    {description: 'A custom storefront powered by Hydrogen'},
+  ];
+}
+export default function Index() {
+  return (
+    <div>
+      <h1>Hello from the home page!</h1>
+    </div>
+  );
+}
